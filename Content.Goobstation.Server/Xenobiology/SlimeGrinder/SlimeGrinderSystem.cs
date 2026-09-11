f572cc9fbe4c0999d54bf20794e873c34fbe79c7
Ceterai <ceterai@protonmail.com>
2026-09-11 03:59:38 +0300

    Fix slime grinder error

--- Content.Goobstation.Server/Xenobiology/SlimeGrinder/SlimeGrinderSystem.cs
+++ Content.Goobstation.Server/Xenobiology/SlimeGrinder/SlimeGrinderSystem.cs
@@ -25,7 +25,7 @@ namespace Content.Goobstation.Server.Xenobiology.SlimeGrinder;
 // -js
 public sealed partial class SlimeGrinderSystem : EntitySystem
 {
-    // [Dependency] private readonly XenobiologySystem _xenobio = default!; // Reserve edit: Fix warnings
+    [Dependency] private readonly XenobiologySystem _xenobio = default!;
     [Dependency] private readonly SharedJitteringSystem _jitteringSystem = default!;
     [Dependency] private readonly SharedAudioSystem _sharedAudioSystem = default!;
     [Dependency] private readonly SharedAmbientSoundSystem _ambientSoundSystem = default!;
